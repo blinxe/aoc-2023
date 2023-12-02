28aa1c1539cb50c222ec1209c77ebcc4168779d0
blx <benoit.linxe@gmail.com>
2023-12-02 17:17:42 +0100

    Move pro solution to its own file

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -114,33 +114,3 @@ mod test {
         super::part_2();
     }
 }
-
-#[cfg(test)]
-fn pro_solution(input: &str, is_part_2: bool) -> u32 {
-    input
-        .lines()
-        .filter(|line| !line.is_empty())
-        .map(|line| {
-            if is_part_2 {
-                line.to_string()
-                    .replace("one", "one1one")
-                    .replace("two", "two2two")
-                    .replace("three", "three3three")
-                    .replace("four", "four4four")
-                    .replace("five", "five5five")
-                    .replace("six", "six6six")
-                    .replace("seven", "seven7seven")
-                    .replace("eight", "eight8eight")
-                    .replace("nine", "nine9nine")
-            } else {
-                line.to_string()
-            }
-        })
-        .map(|line| {
-            line.chars()
-                .filter_map(|c| c.to_digit(10))
-                .collect::<Vec<u32>>()
-        })
-        .map(|vec| 10 * vec.first().unwrap() + vec.last().unwrap())
-        .sum()
-}
